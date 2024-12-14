b900c18eb482862b938c2f1bca7df8ff0fd21d0a
Joel Wiesmann <joel.wiesmann@gmail.com>
2024-12-15 00:55:15 +0100

    Disable SSR as we go for static page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
   },
+  ssr: false,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/image', '@nuxtjs/color-mode'],
